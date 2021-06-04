46b4b13e48d191e2de6bf36c19d8927f28eaa25b
Daniel <daniel.augusto.tonetti@gmail.com>
2021-06-04 12:24:02 -0300

    Create a new model for category app

---
+++ category/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from stdimage import StdImageField
+from uuid import uuid4
+
+
+def new_filename(_instance, filename):
+    ext = filename.split('.')
+    return f'{uuid4()}.{ext}'
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, unique=True)
+    category_description = models.TextField(max_length=250, blank=True)
+    category_image = StdImageField(upload_to=new_filename)
+    slug = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.category_name
