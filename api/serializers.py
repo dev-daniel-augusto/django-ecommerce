285fdd7cb87b49283e0581ab4824cb46dd0d88d6
Daniel <daniel.augusto.tonetti@gmail.com>
2021-06-14 16:47:33 -0300

    Insert serializers file

---
+++ api/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from store.models import Product
+
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Product
+        fields = (
+                'id',
+                'product_name',
+                'product_description',
+                'is_available',
+                'category',
+                'price',
+                'image',
+                'stock',
+                'slug',
+                 )
+        
