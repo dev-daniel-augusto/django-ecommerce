1a1c1a4df7bfbdc0bfc18d72e9fc1474a20e424b
Daniel <daniel.augusto.tonetti@gmail.com>
2021-06-19 12:57:17 -0300

    Implement add product function

--- carts/views.py
+++ carts/views.py
@@ -9,3 +9,25 @@ def _get_cart_id(request):
     if not cart_id:
         cart_id = request.session.create()
     return cart_id
+
+
+def add_product_to_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    try:
+        cart = Cart.objects.get(cart_id=_get_cart_id(request))
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(cart_id=_get_cart_id(request))
+    cart.save()
+    try:
+        cart_item = CartItem.objects.get(product=product,
+                                         cart=cart,
+                                         )
+        cart_item.quantity = cart_item.quantity + 1
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(
+            product=product,
+            cart=cart,
+            quantity=1,
+        )
+    cart_item.save()
+    return redirect('cart')
