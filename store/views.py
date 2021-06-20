7e182b32c2762f94c47484227c30677847631d51
Daniel <daniel.augusto.tonetti@gmail.com>
2021-06-20 11:25:21 -0300

    Create ProductView class

--- store/views.py
+++ store/views.py
@@ -3,8 +3,8 @@ from django.views.generic import TemplateView
 from django.db.models import Q
 from category.models import Category
 from carts.models import CartItem
-from carts.views import _get_cart_id
 from .models import Product
+from carts.views import _get_cart_id
 
 
 class StoreView(TemplateView):
@@ -25,3 +25,18 @@ class StoreView(TemplateView):
             context['products_count'] = products_count
             context['categories'] = Category.objects.all()
             return context
+
+
+class ProductView(TemplateView):
+    template_name = 'product_detail.html'
+
+    def get_context_data(self, category_slug=None, product_slug=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        try:
+            product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+            in_cart = CartItem.objects.filter(cart__cart_id=_get_cart_id(self.request), product=product).exists()
+            context['product'] = product
+            context['added'] = in_cart
+        except Exception as error:
+            raise error
+        return context
