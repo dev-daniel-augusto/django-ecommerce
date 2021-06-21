5aca50ba4bd95a9a92b4ea6ffd2cf79a9cfd97cc
Daniel <daniel.augusto.tonetti@gmail.com>
2021-06-21 09:11:19 -0300

    Change IndexView's TemplateView to ListView

--- home/views.py
+++ home/views.py
@@ -1,5 +1,10 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+
+from store.models import Product
 
 
-class IndexView(TemplateView):
+class IndexView(ListView):
+    models = Product
+    queryset = Product.objects.order_by('price').all().filter(is_available=True)
+    context_object_name = 'products'
     template_name = 'index.html'
