fa317a15aa1c2e4eab8a6f063f1d68b85db12d91
Daniel <daniel.augusto.tonetti@gmail.com>
2021-06-21 09:13:59 -0300

    Add search path

--- store/urls.py
+++ store/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
+
 from .views import (
                     StoreView,
                     ProductView,
+                    SearchView,
                     )
+
 
 urlpatterns = [
     path('', StoreView.as_view(), name='store'),
-    path('<slug:category_slug>/', StoreView.as_view(), name='products_by_category'),
-    path('<slug:category_slug>/<slug:product_slug>/', ProductView.as_view(), name='single_product'),
+    path('category/<slug:category_slug>/', StoreView.as_view(), name='products_by_category'),
+    path('category/<slug:category_slug>/<slug:product_slug>/', ProductView.as_view(), name='single_product'),
+    path('search/', SearchView.as_view(), name='search'),
 ]
