e2d87ad512e56eb68eb1a8f5703edc57e8b59308
Daniel Augusto <daniel.augusto.tonetti@gmail.com>
2021-06-25 11:06:35 -0300

    Insert forms.py file and create UserRegistrationForm

---
+++ user/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+
+from .models import User
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'password',
+            'confirm_password',
+        )
