b8ed4a526fb036cb0837691af004197d0bc90731
Daniel Augusto <daniel.augusto.tonetti@gmail.com>
2021-07-06 13:51:07 -0300

    Update the urls from user app

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import RegisterView, LoginView, LogoutView
+from .views import RegisterView
+from . import views
 
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout')
+    path('login/',  views.login, name='login'),
+    path('logout/', views.logout, name='logout')
 ]
